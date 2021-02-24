72108745d0976248a693545264d92c8be8be04fa
Peter Williams <pedroeldiablo@yahoo.com>
2021-02-24 11:45:42 +0000

    add proxy server to address intermitent CORS issue when fetching on frontend

--- src/utils/fetchProfiles.ts
+++ src/utils/fetchProfiles.ts
@@ -1,6 +1,4 @@
 import btoa from 'btoa';
-// import { error } from 'console';
-
 export interface Profile {
   uuid: string;
   company: string;
@@ -9,28 +7,43 @@ export interface Profile {
   avatar: string;
 }
 
-let uri = 'https://hiring.rewardgateway.net/list';
+let uri = 'http://localhost:8080/https://hiring.rewardgateway.net/list';
 
 let headers = new Headers();
 headers.append('Accept', 'application/json');
-let encoded = btoa(`${process.env.USERNAME}:${process.env.PASSWORD}`);
-console.log({ encoded });
-let auth = 'Basic ' + encoded;
+// let encoded = btoa(`hard:hard`);
+// console.log({ encoded });
+// let encoded = base64;
+let envVar = btoa(
+  `${process.env.REACT_APP_USERNAME}:${process.env.REACT_APP_PASSWORD}`
+);
+// console.log('process', process.env);
+// console.log(process.env.REACT_APP_USERNAME);
+// let encoded = 'aGFyZDpoYXJk';
+let auth = 'Basic ' + envVar;
 headers.append('Authorization', auth);
-headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-console.log(auth);
-console.log(headers);
+headers.append('Access-Control-Allow-Origin', 'http://localhost:5000');
+// console.log({ envVar });
+// console.log({ encoded });
+// console.log(auth);
+// console.log(headers);
 
 let req = new Request(uri, {
   method: 'GET',
   headers,
-  credentials: 'same-origin',
+  // credentials: 'same-origin',
 });
 //credentials: 'same-origin'
+
+console.log({ req });
 
 export function fetchProfiles<T>(): Promise<T> {
   return fetch(req)
     .then(handleErrors)
+    .then((response) => {
+      console.log(response);
+      return response;
+    })
     .then((response) => response.json())
     .then((json) => {
       return json;
