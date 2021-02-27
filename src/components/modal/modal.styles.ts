9050d12f1f646a8588bad857dbc7f198453e67ae
Peter Williams <pedroeldiablo@yahoo.com>
2021-02-27 20:35:29 +0000

    Add additional notes and ensure picure src correct

--- src/components/modal/modal.styles.ts
+++ src/components/modal/modal.styles.ts
@@ -20,5 +20,6 @@ export const Modal = styled.div`
     /* margin: 60px auto; */
     box-shadow: 3px 5px 7px rgba(0, 0, 0, 0.5);
     border: 3px solid white;
+    background-color: #ffffff;
   }
 `;
--- src/components/profile-picture/profile-picture.component.tsx
+++ src/components/profile-picture/profile-picture.component.tsx
@@ -7,15 +7,20 @@ export function ProfilePictureComponent({ avatar, title }: ProfileImage) {
   const [status, image] = useImage(avatar);
 
   if (status === Status.LOADING) {
-    return <div className="spinner" />;
+    return <div className="spinner">Spinner</div>;
   }
 
   let source;
   if (status === Status.FAILED) {
     source = fallback;
   } else {
-    source = avatar;
+    source = image.currentSrc;
   }
 
-  return <img src={source ? source : fallback} alt={`Profile of ${title}`} />;
+  return (
+    <>
+      <p>WTF</p>
+      <img src={source ? source : fallback} alt={`Profile of ${title}`} />
+    </>
+  );
 }
